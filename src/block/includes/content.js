3e68fb568d41ef5ce659c3299bab7f35022015d1
Subrata Sarkar <ssarkar@emfluence.com>
2020-07-22 17:35:01 +0530

    Styling backend

--- src/block/includes/content.js
+++ src/block/includes/content.js
@@ -7,21 +7,47 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { RichText } = wp.editor;
+const { InnerBlocks } = wp.blockEditor;
 
 function emfl_accordion_control() {
 
 
     registerBlockType('emfl/accordion-content', {
         title: __( 'Accordion Item' ),
-        icon: 'shield',
+        icon: 'menu-alt',
         category: 'layout',
         keywords: [],
-        attributes: {},
+        attributes: {
+            itemHeading: {
+                type: 'string',
+                source: 'text',
+                selector: 'a',
+                default: ''
+            }
+        },
 
         edit: (props) => {
+
+            const itemHeading = props.attributes.itemHeading;
+
+            function onChangeItemHeading(content) {
+                props.setAttributes({ itemHeading: content });
+            }
+
             return(
                 <div className={props.className}>
-                    <p>Hellow!!!</p>
+                    <div className="accord-item-container">
+                        <RichText
+                            tagName="a"
+                            className="accord-item-heading"
+                            value={itemHeading}
+                            onChange={onChangeItemHeading}
+                            placeholder={ __( 'Add item heading' ) }
+                        />
+                        <div className="accord-content">
+                            <InnerBlocks allowedBlocks={ ['core/paragraph', 'core/list', 'core/image', 'core/video'] } />
+                        </div>
+                    </div>
                 </div>
             );
         },
